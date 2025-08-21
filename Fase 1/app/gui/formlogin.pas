d39e79dba414a744109b1a00221d7125eb302d31
ricardious <castaneda.systems@gmail.com>
2025-08-21 13:11:16 -0600

    feat(login): enhance login form with new functionalities
    - Hover effect on Sign Up label
    - Cursor change to hand pointer for interactive elements
    - Password masking improvement

--- Fase 1/app/gui/formlogin.pas
+++ Fase 1/app/gui/formlogin.pas
@@ -5,22 +5,28 @@ unit FormLogin;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
+  BCButton, BGRACustomDrawn;
 
 type
 
   { TLogin }
 
   TLogin = class(TForm)
+    BCButton1: TBCButton;
     Edit1: TEdit;
     Edit2: TEdit;
     Image1: TImage;
     Image2: TImage;
     Image3: TImage;
+    Label1: TLabel;
+    LblSignUp: TLabel;
     Shape1: TShape;
     Shape2: TShape;
     Shape3: TShape;
     procedure Edit2Change(Sender: TObject);
+    procedure LblSignUpMouseEnter(Sender: TObject);
+    procedure LblSignUpMouseLeave(Sender: TObject);
   private
 
   public
@@ -39,6 +45,20 @@ implementation
 procedure TLogin.Edit2Change(Sender: TObject);
 begin
      Edit2.PasswordChar := '*';
+end;
+
+procedure TLogin.LblSignUpMouseEnter(Sender: TObject);
+begin
+     LblSignUp.Font.Color := TColor($00FF3C00);
+     LblSignUp.Font.Style := [fsUnderline];
+     LblSignUp.Cursor := crHandPoint;
+end;
+
+procedure TLogin.LblSignUpMouseLeave(Sender: TObject);
+begin
+     LblSignUp.Font.Color := TColor($00FF3C00);
+     LblSignUp.Font.Style := [];
+     LblSignUp.Cursor := crDefault;
 end;
 
 end.
